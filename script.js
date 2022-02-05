df65c51b70315a22074bd3380d446df74969f9b1
TigranGev89 <tigran.sgevorgyan.89@gmail.com>
2022-02-05 20:34:40 +0400

    added script

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const btn = document.getElementsByTagName("button");
+const btn = document.querySelector("#btn");
+const counter = document.querySelector("#count");
+let count = 0;
+btn.addEventListener("click", ()=>{
+    count +=1;
+    counter.innerHTML = `${count}`
+})
